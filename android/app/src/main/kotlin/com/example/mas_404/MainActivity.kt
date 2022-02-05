1e5a5d18f64de4603ee4e3730a5ebfae50e433d9
sabirklm <mondal.sabir.com97@gmail.com>
2022-02-05 20:25:40 +0530

    mas404 vrsion1 created

---
+++ android/app/src/main/kotlin/com/example/mas_404/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mas_404
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'pages/pages.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyCzb6YFVoyfQ60FfRKCWY7ai7y0iTKTnbY",
+        appId: "1:572194537793:web:447169984265a29e4621c0",
+        messagingSenderId: "572194537793",
+        projectId: "insion"),
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BODMAS',
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
